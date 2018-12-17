3de780a6a196de8a70b5f91534b521bd7657134e
Osky772 <kowalow.oskar@gmail.com>
2018-12-17 19:08:19 +0100

    find items and remove them
    Added a funcionality of finding element in array "chooseRightOnes"
    and when found then remove that html element. Quite nice :D

--- ROMA-gra-Oskar-Kowalów/script.js
+++ ROMA-gra-Oskar-Kowalów/script.js
@@ -1,35 +1,73 @@
 const container = document.getElementById("container");
 const box = document.getElementById("box");
-const animal = document.querySelector(".animal")
-
+const howManyElements = 5; //level of hardness
+let countElements = 0; //for setInterval 
+let chooseRightOnes = ["bird", "lion", "pinguin"]; //array of items to find
+const animals = [
+    {
+        icon: "🦁",
+        id: "lion",
+    }, 
+    {
+        icon: "🐮",
+        id: "cow",
+    }, 
+    {
+        icon: "🐷",
+        id: "piggy",
+    },
+    {
+        icon: "🐸",
+        id: "flog",
+    },
+    {
+        icon: "🐧",
+        id: "pinguin",
+    },
+    {
+        icon: "🐦",
+        id: "bird",
+    },
+];
 
 function createAnimals() {
-    const animals = ["🐍", "🦁", "🐮", "🐷", "🐽", "🐸", "🐵", "🙈", "🙉", "🙊", "🐒", "🐔", "🐧", "🐦", "🐤", "🐣", "🐥",];
     const index = Math.floor(Math.random() * animals.length);
-    const animal = animals[index];
+    const animal = animals[index].icon;
+    const animalId = animals[index].id;
     const animalElement = document.createElement("div");
     
     animalElement.classList.add("animal");
     animalElement.textContent = animal;
+    animalElement.dataset.id = animalId;
     animalElement.style.top = `${Math.random() * 90}%`; 
     animalElement.style.left = `${Math.random() * 90}%`;
 
     animalElement.addEventListener("click", function() {
-        console.log(this);
-        this.remove();
+        console.log(this.dataset.id);
+
+        // find element by it's is 
+        const isFound = chooseRightOnes.some(function(element){
+            return element === animalElement.dataset.id;
+        });
+
+        // if you found item then remove it
+        if (isFound) {
+            this.remove();
+        };
+
     });
 
     container.prepend(animalElement);
 }
 
-
-
-var counter = 0;
-var animalsInterval = setInterval(function(){
+let animalsInterval = setInterval(function(){
     createAnimals();
-    counter++;
-    if (counter === 3) {    
+    countElements++;
+    if (countElements === howManyElements) {    
         clearInterval(animalsInterval)
     }
 }, 200);
 
+
+
+console.log(document.querySelectorAll('.animal').length)
