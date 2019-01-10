635bc5d7403568c88b6659b05a3c8866bff21f33
Osky772 <kowalow.oskar@gmail.com>
2019-01-10 17:59:07 +0100

    work on css - highlight menu elements

--- js/highlight-menu.js
+++ js/highlight-menu.js
@@ -3,22 +3,22 @@ function trackScroll() {
     const hero = document.getElementById("hero");
     const heroScroll = hero.offsetTop - 60;
     
-    const links = document.querySelectorAll(".main-nav ul a");
+    const links = document.querySelectorAll(".menu__element");
 
     const mainFunc = document.getElementById("main-functions");
-    const mainFuncLink = document.querySelector('[href="#main-functions"]');
+    const mainFuncLink = document.querySelector('.menu__element-main');
     const mainFuncScroll = mainFunc.offsetTop - 60;
 
     const offer = document.getElementById("offer");
-    const offerLink = document.querySelector('[href="#offer"]');
+    const offerLink = document.querySelector('.menu__element-offer');
     const offerScroll = offer.offsetTop - 60;
 
     const form = document.getElementById("form");
-    const formLink = document.querySelector('[href="#form"]');
+    const formLink = document.querySelector('.menu__element-form');
     const formScroll = form.offsetTop - 60;
 
     const about = document.getElementById("about-us");
-    const aboutLink = document.querySelector('[href="#about-us"]');
+    const aboutLink = document.querySelector('.menu__element-about');
     const aboutScroll = about.offsetTop - 60;
     
     let scrolled = window.pageYOffset;
