e468591d3d3b0fa9ff4e238e1b69f1f72dde823f
joanna-by <j.bykowska@gmail.com>
2019-02-08 20:08:31 +0100

    fixing translation bugs and adding flags icons

--- js/translation.js
+++ js/translation.js
@@ -1,6 +1,41 @@
-$('[lang="en"]').hide();
+(function() {
+    const $langButton = $('div.lang-button');
+    const $translationsText = $('[data-lang]');
+    const $translationsFormPlaceholder = $('[data-langplaceholder]');
+    const $translationsFormTitle = $('[data-langtitle]');
+    let prevLang;
 
-$('#switch-lang').click(function() {
-    $('[lang="en"]').toggle();
-    $(':not(html)[lang="pl"]').toggle();
-});
+    function languageSwitch() {
+
+        $translationsText.each(function() {
+            prevLang = $(this).html();
+            $(this).html($(this).attr('data-lang'));
+            $(this).attr('data-lang', prevLang)
+        })
+        $translationsFormPlaceholder.each(function() {
+            prevLang = $(this).attr('placeholder');
+            $(this).attr('placeholder', ($(this).attr('data-langplaceholder')));
+            $(this).attr('data-langplaceholder', prevLang)
+        })
+        $translationsFormTitle.each(function() {
+            prevLang = $(this).attr('title');
+            $(this).attr('title', ($(this).attr('data-langtitle')));
+            $(this).attr('data-langtitle', prevLang)
+        })
+
+        if (localStorage.language === 'english') {
+            localStorage.setItem('language', 'polish');
+        } else {
+            localStorage.setItem('language', 'english');
+        }
+
+    }
+
+    $langButton.click(languageSwitch);
+
+    if (localStorage.language === 'english') {
+        languageSwitch();
+        localStorage.setItem('language', 'english');
+    }
+
+}) ();
